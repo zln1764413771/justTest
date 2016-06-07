49b3daf1bd4b7845e3aa6ac658a507c5d54af09d
zln1764413771 <1764413771@qq.com>
2016-06-07 17:46:49 +0800

    2

--- Just/Just/ViewController.m
+++ Just/Just/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"111111");
+    NSLog(@"222222");
 }
 
 - (void)didReceiveMemoryWarning {
