3156c8c1acbddac83f6860808371a68677240816
zln1764413771 <1764413771@qq.com>
2016-06-07 17:44:50 +0800

    add a project

---
+++ Just/Just/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Just
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 周丽娜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Just/Just/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Just
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 周丽娜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
